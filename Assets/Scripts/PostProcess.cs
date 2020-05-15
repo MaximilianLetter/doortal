e18517ff1974c868b4ac14a7205f15d6e8d41248
MaximilianLetter <maximilian.letter@gmail.com>
2020-05-15 09:08:09 +0200

    Use session state changed callback to match the camera parameters

--- Assets/Scripts/PostProcess.cs
+++ Assets/Scripts/PostProcess.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARCore;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 [ExecuteInEditMode]
 public class PostProcess : MonoBehaviour
@@ -9,17 +12,6 @@ public class PostProcess : MonoBehaviour
     public Camera stencilCam;
 
     private RenderTexture stencilTex;
-    private RenderTexture buffer;
-    //public Material stencilMaterial;
-    //public Material simpleRender;
-
-    //private RenderTexture cameraRenderTexture;
-    //private RenderTexture buffer;
-
-    //private void Start()
-    //{
-    //    GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
-    //}
 
     private void Start()
     {
@@ -31,128 +23,25 @@ public class PostProcess : MonoBehaviour
 
         stencilCam.targetTexture = stencilTex;
 
-        Invoke("MatchMainCamera", 1.0f);
+        // Subscribe to state changed event to match the camera params
+        ARSession.stateChanged += OnStageChanged;
     }
 
-    void MatchMainCamera()
+    void OnStageChanged(ARSessionStateChangedEventArgs stateChange)
     {
-        Camera cam = Camera.main;
-        Debug.Log(cam.projectionMatrix);
+        if (stateChange.state == ARSessionState.SessionTracking)
+        {
+            Camera cam = Camera.main;
+            stencilCam.projectionMatrix = cam.projectionMatrix;
 
-        stencilCam.projectionMatrix = cam.projectionMatrix;
-
-        Debug.Log(stencilCam.projectionMatrix);
+            // Unsubscribe from the event after successful parameter matching
+            ARSession.stateChanged -= OnStageChanged;
+        }
     }
-
-    //private void OnRenderImage(RenderTexture source, RenderTexture destination)
-    //{
-    //    RenderTexture buffer = RenderTexture.GetTemporary(source.width, source.height, 24);
-    //    Graphics.Blit(source, buffer, effectMaterial);
-    //    Graphics.Blit(buffer, destination);
-    //    RenderTexture.ReleaseTemporary(buffer);
-
-    //    //Graphics.Blit(source, destination, effectMaterial);
-    //}
 
     public void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         effectMaterial.SetTexture("_StencilTex", stencilTex);
         Graphics.Blit(source, destination, effectMaterial);
     }
-
-    // NOTE: Version with buffer between
-    //public void OnRenderImage(RenderTexture source, RenderTexture destination)
-    //{
-    //    buffer = RenderTexture.GetTemporary(Screen.width, Screen.height, 24);
-    //    Graphics.Blit(source, buffer);
-
-    //    effectMaterial.SetTexture("_BufferTex", buffer);
-    //    effectMaterial.SetTexture("_StencilTex", stencilTex);
-    //    Graphics.Blit(source, destination, effectMaterial);
-    //}
-
-    //private void OnPostRender()
-    //{
-    //    RenderTexture.ReleaseTemporary(buffer);
-    //}
-
-    //private void OnPostRender()
-    //{
-    //    Graphics.SetRenderTarget(buffer);
-    //    GL.Clear(true, true, Color.black);
-
-    //    Graphics.SetRenderTarget(buffer.colorBuffer, cameraRenderTexture.depthBuffer);
-    //    Graphics.Blit(cameraRenderTexture, simpleRender);
-    //    Graphics.Blit(cameraRenderTexture, effectMaterial);
-
-    //    RenderTexture.active = null;
-    //    Graphics.Blit(buffer, simpleRender);
-    //}
 }
-
-
-// NOTES: does nothing but could be kind of workaround
-//public class PostProcess : MonoBehaviour
-//{
-//    public Material effectMaterial;
-//    private CommandBuffer commandBuffer;
-
-//    private Texture2D m_Texture;
-//    private Texture2D texture
-//    {
-//        get
-//        {
-//            if (m_Texture == null)
-//            {
-//                m_Texture = new Texture2D(16, 16);
-//            }
-
-//            return m_Texture;
-//        }
-//    }
-
-//    private Camera m_Camera;
-//    public Camera camera
-//    {
-//        get
-//        {
-//            if (m_Camera == null)
-//            {
-//                m_Camera = GetComponent<Camera>();
-//            }
-
-//            return m_Camera;
-//        }
-//    }
-
-//    private void Start()
-//    {
-//        if (commandBuffer == null)
-//        {
-//            commandBuffer = new CommandBuffer();
-//            commandBuffer.name = "commandBuffer";
-
-//            int stencilTextureID = Shader.PropertyToID("_StencilTexture");
-//            commandBuffer.GetTemporaryRT(stencilTextureID, -1, -1, 24);
-
-//            commandBuffer.Blit(BuiltinRenderTextureType.None, stencilTextureID, effectMaterial);
-
-//            commandBuffer.SetGlobalTexture("_StencilTexture", stencilTextureID);
-//            camera.AddCommandBuffer(CameraEvent.AfterForwardAlpha, commandBuffer);
-//        }
-//    }
-
-//    private void OnRenderImage(RenderTexture source, RenderTexture destination)
-//    {
-//        RenderTexture stencilRT = Shader.GetGlobalTexture("_StencilTexture") as RenderTexture;
-//        RenderTexture activeRT = RenderTexture.active;
-//        RenderTexture.active = stencilRT;
-
-//        texture.ReadPixels(new Rect(Screen.width / 2, Screen.height / 2, texture.width, texture.height), 0, 0, false);
-//        //Debug.Log (texture.GetPixel(0, 0));
-
-//        RenderTexture.active = activeRT;
-
-//        Graphics.Blit(source, destination);
-//    }
-//}
