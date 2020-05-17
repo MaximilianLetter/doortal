304d3535fe2e92aa30aa540d860ab7754a994526
MaximilianLetter <maximilian.letter@gmail.com>
2020-05-17 11:44:20 +0200

    Scale augmentation buttons if active

--- Assets/Scripts/AugmentationChangeButton.cs
+++ Assets/Scripts/AugmentationChangeButton.cs
@@ -7,14 +7,20 @@ public class AugmentationChangeButton : MonoBehaviour
     public Augmentation augmentation;
 
     private AugmentationManager augmentationManager;
+    private ButtonManager buttonManager;
 
     private void Start()
     {
         augmentationManager = GameObject.FindObjectOfType<AugmentationManager>();
+        buttonManager = transform.parent.gameObject.GetComponent<ButtonManager>();
     }
 
     public void ChangeAugmentation()
     {
         augmentationManager.CurrentAugmentation = augmentation;
+
+        buttonManager.ResetButtons();
+
+        GetComponent<RectTransform>().sizeDelta = buttonManager.activeButtonSize;
     }
 }
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Vector2 buttonSize = new Vector2(150, 150);
+    public Vector2 activeButtonSize = new Vector2(300, 300);
+
+    private RectTransform[] buttons;
+
+    void Start()
+    {
+        buttons = new RectTransform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            buttons[i] = transform.GetChild(i).GetComponent<RectTransform>();
+        }
+
+        // Make sure all buttons are in default size
+        ResetButtons();
+
+        // Set the first element to the default active button
+        buttons[0].sizeDelta = activeButtonSize;
+    }
+
+    public void ResetButtons()
+    {
+        foreach (var button in buttons)
+        {
+            button.sizeDelta = buttonSize;
+        }
+    }
+}
