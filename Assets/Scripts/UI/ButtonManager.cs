096bbdcecccfb642d35215ae7582e5be0e9eaecb
MaximilianLetter <maximilian.letter@gmail.com>
2020-05-29 12:25:32 +0200

    First version of swipeable augmentations

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -8,9 +8,14 @@ public class ButtonManager : MonoBehaviour
     public Vector2 activeButtonSize = new Vector2(300, 300);
 
     private RectTransform[] buttons;
+    private int currentActive = 0;
+
+    private AugmentationManager augmentationManager;
 
     void Start()
     {
+        augmentationManager = GameObject.FindObjectOfType<AugmentationManager>();
+
         buttons = new RectTransform[transform.childCount];
 
         for (int i = 0; i < transform.childCount; i++)
@@ -25,7 +30,25 @@ public class ButtonManager : MonoBehaviour
         buttons[0].sizeDelta = activeButtonSize;
     }
 
-    public void ResetButtons()
+    public void SetActiveButton(int num)
+    {
+        ResetButtons();
+
+        buttons[num].sizeDelta = activeButtonSize;
+
+        // Set active augmentation by casting the given number to Enumeration type
+        augmentationManager.CurrentAugmentation = (Augmentation)num;
+
+        currentActive = num;
+    }
+
+    public void SwipeAugmentation(int direction)
+    {
+        currentActive = Mathf.Clamp(currentActive - direction, 0, buttons.Length - 1);
+        SetActiveButton(currentActive);
+    }
+
+    void ResetButtons()
     {
         foreach (var button in buttons)
         {
---
+++ Assets/SwipeManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeManager : MonoBehaviour
+{
+    private float minSwipeDist = 50.0f;
+
+    private float startTime;
+    private Vector2 startPos;
+
+    private bool swipePossible;
+
+    public ButtonManager btnManager;
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    swipePossible = true;
+                    startPos = touch.position;
+                    startTime = Time.time;
+                    break;
+
+                case TouchPhase.Stationary:
+                    swipePossible = false;
+                    break;
+
+                case TouchPhase.Ended:
+                    float swipeTime = Time.time - startTime;
+                    float swipeDist = (touch.position - startPos).magnitude;
+
+                    if (swipePossible && (swipeDist > minSwipeDist))
+                    {
+                        int swipeDirection = (int)Mathf.Sign(touch.position.y - startPos.y);
+                        Debug.Log("swipe");
+                        Debug.Log(swipeDirection);
+
+                        btnManager.SwipeAugmentation(swipeDirection);
+                    }
+                    break;
+
+            }
+        }
+    }
+
+}
