a9f5da47b31d8495c2ad9a34e58941ae35c649e4
MaximilianLetter <maximilian.letter@gmail.com>
2020-06-09 12:22:42 +0200

    Show fps in debug text

---
+++ Assets/ShowFPS.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowFPS : MonoBehaviour
+{
+    private Text text;
+    float deltaTime = 0.0f;
+    int frames = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frames++;
+
+        deltaTime += Time.deltaTime;
+        if (deltaTime >= 1.0f)
+        {
+            text.text = "FPS: " + frames;
+
+            deltaTime = 0.0f;
+            frames = 0;
+        }
+    }
+}
