485e02100b8f28223e7881b72e125f7d0c2691a9
MaximilianLetter <maximilian.letter@gmail.com>
2020-06-13 09:08:58 +0200

    Stop tap on screen if it hits UI

--- Assets/Scripts/UI/TouchManager.cs
+++ Assets/Scripts/UI/TouchManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TouchManager : MonoBehaviour
 {
     public float minSwipeDist = 25.0f;
+    public float maxTime = 2.0f;
 
     private float startTime;
     private Vector2 startPos;
@@ -42,18 +44,35 @@ public class TouchManager : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
+
+                    // Make sure that touch does not start on game object
+                    if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                    {
+                        singleTouchPossible = false;
+                        swipePossible = false;
+                        return;
+                    }
+
                     swipePossible = true;
+                    singleTouchPossible = true;
+
                     startPos = touch.position;
                     startTime = Time.time;
                     break;
 
                 case TouchPhase.Stationary:
                     swipePossible = false;
-                    singleTouchPossible = true;
                     break;
 
                 case TouchPhase.Ended:
-                    float swipeTime = Time.time - startTime;
+
+                    float touchTime = Time.time - startTime;
+
+                    if (touchTime > maxTime)
+                    {
+                        return;
+                    }
+
                     float swipeDist = Mathf.Abs(touch.position.x - startPos.x);
 
                     if (swipePossible && (swipeDist > minSwipeDist))
@@ -67,12 +86,31 @@ public class TouchManager : MonoBehaviour
                     else if (singleTouchPossible)
                     {
                         Debug.Log("TOUCH for PORTAL");
+
+                        // Version2
+                        
                         portalManager.SpawnPortal();
+
+                        // Version1
+                        //if (!IsOnUI())
+                        //{
+                        //    portalManager.SpawnPortal();
+                        //}
                     }
                     break;
 
             }
         }
+    }
+
+    private bool IsOnUI()
+    {
+        PointerEventData eventDataPos = new PointerEventData(EventSystem.current);
+        eventDataPos.position = Input.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataPos, results);
+
+        return results.Count > 0;
     }
 
 }
